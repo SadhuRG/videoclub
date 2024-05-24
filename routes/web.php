d89525db600460f0ad50195a80d40a28f9df5476
SadhuRG <Andresrojas1234321@gmail.com>
2024-05-24 01:18:42 -0500

    TRABAJO 01

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+
+//Petición por get
+
+Route::get('/', function () {
+	return '¡Hola Mundo desde Laravel..!';
+});
+
+//Petición por post
+
+Route::post('persona/nuevo', function(){
+	return '¡Hola Mundo desde Laravel..!';
+});
+
+//Petición por put
+
+Route::put('persona/nuevo', function () { 
+	//
+});
+
+//Petición por delete
+
+Route::delete('persona/borrar', function () {
 });
