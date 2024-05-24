638c79c372fc4de56a338c661bd50da59893c195
SadhuRG <Andresrojas1234321@gmail.com>
2024-05-24 11:23:11 -0500

    TRABAJO 2

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('content')
+    <h1 class="text-3xl m-6 text-center" >Pantalla principal</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,15 @@ Route::get('/', function () {
 
 //Petición por get
 
+/*
 Route::get('/', function () {
 	return '¡Hola Mundo desde Laravel..!';
 });
+*/
 
 //Petición por post
 
+/*
 Route::post('persona/nuevo', function(){
 	return '¡Hola Mundo desde Laravel..!';
 });
@@ -30,3 +33,45 @@ Route::put('persona/nuevo', function () {
 
 Route::delete('persona/borrar', function () {
 });
+
+
+Route::match(array('GET' , 'POST'), '/', function(){
+    return '!Hola Mundo desde laravel...!';
+}); 
+
+
+route::get('persona/{nombre?}', function($nombre = null){
+	return $nombre;
+});
+
+*/
+
+
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+
+Route::get('/catalog', function () {
+    return view('catalog.index');
+});
+
+Route::get('/catalog/show/{id}', function ($id) {
+    return view('catalog.show', ['id' => $id]);
+});
+
+Route::get('/catalog/create', function () {
+    return view('catalog.create');
+});
+
+Route::get('/catalog/edit/{id}', function ($id) {
+    return view('catalog.edit', ['id' => $id]);
+});
+
+
+
